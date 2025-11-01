93ce52df6d00fbf9a2adae20a9c4cd4f74147bb2
LatteLorenso <antonreserve67@gmail.com>
2025-11-01 13:24:08 +0500

    Закончил логику кнопок JS!

--- src/js/devpagescript.js
+++ src/js/devpagescript.js
@@ -3,26 +3,74 @@
 // id="btn-1200rub"
 // id="btn-2400rub"
 
-const btn300 = document.getElementById('btn-300rub');
-const btn800 = document.getElementById('btn-800rub');
-const btn1200 = document.getElementById('btn-1200rub');
-const btn2400 = document.getElementById('btn-2400rub');
+const tarifBtns = document.querySelectorAll('.btn-tariff');
 
 const inputemail = document.getElementById('input-email');
 const btnsubmit = document.getElementById('btn-submit');
 
-btn300.addEventListener("click", () => {
-    btn300.classList.add("active");
+let emailCheck = false;
+let tarifCheck = false;
+let selectedTarif = "";
 
-    if (!inputemail.value) {
+tarifBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+    // при нажатии на любую кнопку
+    tarifCheck = true;
+
+    // снять активный класс со всех
+    tarifBtns.forEach(b => b.classList.remove("active"));
+
+    // добавить активный класс только для нажатой
+    btn.classList.add("active");
+
+    // сохранить выбранный тариф
+    selectedTarif = btn.textContent.split('-')[1].trim();
+
+    checkButton();
+});
+});
+
+inputemail.addEventListener("input", () => {
+  if (inputemail.value.includes("@")) {
+    emailCheck = true;
+  } else {
+    emailCheck = false;
+  }
+
+  checkButton();
+});
+
+function checkButton() {
+    if (emailCheck && tarifCheck) {
+        btnsubmit.classList.add("active");
+        btnsubmit.textContent = `Оформить подписку - ${selectedTarif}`;
+    } else if (!emailCheck) {
         btnsubmit.textContent = "Введите корректный email";
         btnsubmit.classList.remove("active");
-    } else if (inputemail.value.includes("@")) {
-        btnsubmit.textContent = `Оформить подписку - ${"300 руб"}`;
-        btnsubmit.classList.add("active");
-    } else {
-        btnsubmit.textContent = "Введите корректный email";
+    } else if (!tarifCheck) {
+        btnsubmit.classList.remove("active");
     }
+}
 
-    }
-)
+
+// btn300.addEventListener("click", () => {
+//     btn300.classList.add("active");
+
+//     if (!inputemail.value) {
+//         btnsubmit.textContent = "Введите корректный email";
+//         btnsubmit.classList.remove("active");
+//     } else if (inputemail.value.includes("@")) {
+//             btnsubmit.textContent = `Оформить подписку - ${"300 руб"}`;
+//             btnsubmit.classList.add("active");
+//         }
+//     })
+    
+        
+    // else if (inputemail.value.includes("@")) {
+    //     btnsubmit.textContent = `Оформить подписку - ${"300 руб"}`;
+    //     btnsubmit.classList.add("active");
+    // } else {
+    //     btnsubmit.textContent = "Введите корректный email";
+    // }
+
+// Закончил здеся 
